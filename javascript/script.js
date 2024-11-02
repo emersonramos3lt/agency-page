d8a16256140c10a4fb8a70d79e84103756c73b7b
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2024-11-02 15:19:57 -0300

    New

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+const mainMenu = document.getElementById("mainMenu");
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const items = document.querySelectorAll("nav .mainMenu li a");
+const header = document.querySelector("header");
+const logo = document.getElementById("logo");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+items.forEach(item => {
+    item.addEventListener("click", function() {
+        close();
+    });
+});
+
+function show() {
+    mainMenu.style.display = "flex";
+    mainMenu.style.top = "0";
+    document.body.style.overflow = "hidden";
+    header.style.backgroundColor = "#fff";
+    openMenu.style.color = "#fff"
+    logo.style.color = "#fff";
+}
+
+function close() {
+    mainMenu.style.top = "-110%";
+    document.body.style.overflow = "auto";
+    header.style.backgroundColor = "#003049";
+    openMenu.style.color = "#fff";
+    logo.style.color = "#fff"
+}
